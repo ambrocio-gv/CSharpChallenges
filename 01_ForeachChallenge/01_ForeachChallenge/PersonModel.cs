58b9512019beed6e378b22d53ce972a5bd2eac80
ambrocio-gv <56829483+ambrocio-gv@users.noreply.github.com>
2021-06-17 17:44:41 +0800

    01 - foreach challenge

---
+++ 01_ForeachChallenge/01_ForeachChallenge/PersonModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ForeachChallenge
+{
+    class PersonModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{ FirstName } { LastName }";
+            }
+        }
+        
+
+
+
+    }
+}
---
+++ 01_ForeachChallenge/01_ForeachChallenge/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_ForeachChallenge
+{
+    class Program
+    {
+        //Create a Console app that has a
+        //List<string> populated with a set of
+        //names. Loop through the names using 
+        //the foreach. For every name, print it
+        //to the console.
+
+        static void Main(string[] args)
+        {
+
+            foreach (string person in GetPersonString())
+            {
+                Console.WriteLine(person);
+            }
+
+            foreach (PersonModel personModel in GetPersonModel())
+            {
+                Console.WriteLine(personModel.FullName);
+            }
+
+            Console.ReadKey();
+        }
+
+
+        private static List<string> GetPersonString() =>
+            new List<string>()
+            {
+                "Gerard",
+                "Car",
+                "Bev",
+                "Will"
+                
+            };
+
+
+        private static List<PersonModel> GetPersonModel() =>
+            new List<PersonModel>()
+            {
+                new PersonModel { FirstName="Anthony", LastName="Becker" },
+                new PersonModel { FirstName="Cam", LastName="DeGuzman" },
+                new PersonModel { FirstName="Fiona", LastName="Gil" },
+                new PersonModel { FirstName="Hanna", LastName="Ibrahim" },
+            };
+
+        
+
+
+
+
+    }
+}
